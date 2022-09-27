99b94d9ae924db55ea4adc1e187a06548f6a8a35
shinyxu <114185773+shinyxu@users.noreply.github.com>
2022-09-27 13:38:28 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -41,7 +41,7 @@ export const USER_CONFIG = {
     poetry: false,
     
     /** 星座运势 */
-    horoscope: false,
+    horoscope: true,
     
     /** 生日消息和节日消息 */
     birthdayMessage: true,
@@ -64,7 +64,7 @@ export const USER_CONFIG = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "o9F_m6InLYJOj2PaGMAmt88O3kYg",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "dtov484JKvJEQOckZ6PJV5yGJyhicSkNpEZotvHzCT4",
+      useTemplateId: "74I0m3Os43yB2n_mbqhZezKXDPq9nQCqOIvmPdC9pSY",
       // 所在省份
       province: "广东",
       // 所在城市
